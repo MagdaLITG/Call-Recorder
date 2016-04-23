632710c511ffb804bb24ff2dc17550d557c555f8
MagdaLITG <imagda95@gmail.com>
2016-04-23 21:58:58 +0200

    Added files via upload

---
+++ DeviceAdminDemo.java
@@ -0,0 +1,21 @@
+package com.example.android.callrecorder;
+
+import android.app.admin.DeviceAdminReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+/**
+ * Created by Magda on 2016-04-23.
+ */
+public class DeviceAdminDemo extends DeviceAdminReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        super.onReceive(context, intent);
+    }
+
+    public void onEnabled(Context context, Intent intent) {
+    };
+
+    public void onDisabled(Context context, Intent intent) {
+    };
+}
